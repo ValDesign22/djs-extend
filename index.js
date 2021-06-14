a18ef1bc41dec5ed717b5a89a15eea8074975124
ValRedstone <valdorner31@gmail.com>
2021-06-14 12:23:19 +0200

    New Version

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const { Client } = require("discord.js");
+
 function Login(client, token) {
     this.client = client;
     this.token = token;
 
-    
+    this.client = new Client()
+
+    this.client.login(this.token)
+}
+
+module.exports = {
+    Login
 }
